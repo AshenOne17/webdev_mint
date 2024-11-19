0dbc3ac48839a1eb2555441ad48099e9db459363
Ashenone17 <artemgalan02@gmail.com>
2024-11-19 21:44:36 +0100

    Updated.

---
+++ assets/scripts/index.js
@@ -0,0 +1,13 @@
+/**
+ * index.js
+ * - All our useful JS goes here, awesome!
+ */
+
+console.log("JavaScript is amazing!");
+
+$('.has-tip').foundation();
+
+$(document).ready(function(){
+	$('body').css('display', 'none');
+	$('body').fadeIn(1500);
+})
